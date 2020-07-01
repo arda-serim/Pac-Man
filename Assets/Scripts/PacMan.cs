40546ba8e84b9263e375f046ffa0cfdcf2fb8952
Arda Serim <arserim20@gmail.com>
2020-07-01 13:45:52 +0300

    PacMan Script

---
+++ Assets/Scripts/PacMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMan : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Turn()
+    {
+
+    }
+}
