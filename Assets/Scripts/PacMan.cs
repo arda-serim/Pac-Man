3de4ad4709b94b11785d50677e177c112fa260de
Arda Serim <arserim20@gmail.com>
2020-07-01 14:51:50 +0300

    Pac-Man

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PacMan : MonoBehaviour
 {
+    Rigidbody2D rb;
+    [SerializeField]int speed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        MoveForward();
+    }
+
+    void MoveForward()
+    {
+        Debug.Log(transform.position + transform.right);
+        rb.MovePosition(transform.position + transform.right * Time.deltaTime * speed);
     }
 
     void Turn()
