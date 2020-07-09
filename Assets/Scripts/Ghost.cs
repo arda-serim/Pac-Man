e464de6956fcc1ed3de537dd7444691c8f200aef
Berkin Serim <brknsrm0@gmail.com>
2020-07-10 00:30:24 +0300

    Ghost

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ghost : MonoBehaviour
+{
+    private int speed;
+    private Rigidbody2D rb;
+    private Vector2
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+    void MoveForward()
+    {
+        rb.MovePosition(transform.position + transform.right * Time.deltaTime * speed);
+    }
+}
