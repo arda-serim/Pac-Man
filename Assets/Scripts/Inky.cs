0b9da1e8451a52ac77be584ca3f464a1facae825
Arda Serim <arserim20@gmail.com>
2020-07-13 19:34:18 +0300

    Inky

---
+++ Assets/Scripts/Inky.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inky : Ghost
+{
+    GameObject blinky = GameObject.Find("Blinky");
+
+
+    public override Vector3 SetWaypoint()
+    {
+        if (phase == Phase.Dead)
+        {
+            return new Vector3(0, 0.35f);
+        }
+
+        if (phase == Phase.Scatter)
+        {
+            return new Vector3(4.059f, -4.466f);
+        }
+
+        return pacman.transform.position + pacman.transform.right * 0.5f - blinky.transform.position;
+    }
+
+
+}
